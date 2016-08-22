46af9a8869f226d081f48d291c9f6ce815b4f2d9
akrherz <akrherz@iastate.edu>
2016-08-22 15:39:15 -0500

    correct another typo

--- database/bootstrap.sh
+++ database/bootstrap.sh
@@ -2,4 +2,4 @@
 psql -c "create database talltowers;" -U postgres
 psql -f createroles.sql -U postgres
 psql -U tt_admin -f createtables.sql talltowers
-psql -U tt_admin -f table-dat-partititon.sql talltowers
+psql -U tt_admin -f table-dat-partition.sql talltowers
