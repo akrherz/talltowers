98bf82a8d48e115b96c864372cc2798df7042361
akrherz <akrherz@iastate.edu>
2016-08-25 07:51:24 -0500

    update plotting script to use new schema

--- plots/baro.py
+++ plots/baro.py
@@ -12,7 +12,7 @@ import json
 import datetime
 from dateutil import tz
 import psycopg2
-import pandas as pd
+from pandas.io.sql import read_sql
 import matplotlib
 matplotlib.use('Agg')  # Must be before importing matplotlib.pyplot.
 import matplotlib.pyplot as plt  # NOPEP8
@@ -22,7 +22,7 @@ CONFIG = json.load(open("../config/settings.json", 'r'))
 
 # === INPUTS ===
 
-site_list = ['story', 'hamilton']
+site_list = ['hamilton', 'story']
 hours_back = 36
 plot_dir = CONFIG['plotsdir']
 
@@ -40,87 +40,61 @@ for key, value in plot_dict.items():
     ts_now_local = datetime.datetime.strftime(
         time_now.astimezone(tz.gettz('US/Central')), "%d-%b-%y %H:%M %Z")
     dfs = []
-    for site in site_list:
-        conn = psycopg2.connect(('host={hostname} dbname={dbname} '
-                                 'user={dbuser} password={dbpass}'
-                                 ).format(**CONFIG['dbconn']))
-        cur = conn.cursor()
-        for chn in value['channels']:
-            # get chn_id
-            cur.execute("""SELECT chn_id
-                           FROM Channels
-                           WHERE site = %s
-                           AND header = %s;""", (site, chn))
-            chn_id = cur.fetchall()[0][0]
-            # get data
-            #   note: datetime.datetime objects with a tz, will be
-            #       converted to postgres's "TIMESTAMPTZ" format
-            cur.execute("""SELECT date_trunc('minute', ts), AVG(val)
-                        FROM Dat
-                        WHERE chn_id = %s
-                        AND ts > %s
-                        AND ts < %s
-                        GROUP BY date_trunc('minute', ts)
-                        ORDER BY date_trunc('minute', ts);
-                        """, (chn_id, time_data_start, time_now))
-            data = cur.fetchall()
-            # make pandas.DataFrame object
-            df_raw = pd.DataFrame(data, columns=['ts',
-                                                 chn]).set_index('ts')
-            df_raw.columns = ["_".join([site.capitalize(),
-                                        df_raw.columns[0]])]
-            dfs.append(df_raw)
-    # ===================== PLOT DATA, IF ANY =======================
-    # verify that data is present
-    if any([len(dfx) > 0 for dfx in dfs]):
-        # combine all channels into one data frame
-        df = pd.concat(dfs, axis=1)
-        # verify that dat is not all NANs
-        if len(df.dropna(how='all') > 0):
-            # === PLOT ===
-            fig, ax = plt.subplots(figsize=(17, 11))
-            ts = df.index
-            for col in df:
-                ax.plot(ts, df[col], label=col)
-            # set legend and titles
-            lgnd = ax.legend(loc='best')
-            plot_title = ("One minute average of last {} hours of {} from BOTH"
-                          ).format(hours_back, key, site.upper())
-            ax.set_title(plot_title, fontsize=22)
-            # set texts with times
-            fig.text(x=0.98, y=0.99, s='   generated at   \n' + ts_now_local,
-                     color='#888888', ha='right', va='top', fontsize=12)
-            ts_db_last = df.index[-1].astimezone(
-                tz.gettz('UTC')).strftime('%d-%b-%y %H:%M UTC')
-            fig.text(x=0.98, y=0.01,
-                     s=('newest data:' + ts_db_last +
-                        '\nquery at: ' + ts_query_utc),
-                     color='#888888', ha='right', va='bottom', fontsize=10)
-            # set all tick parameters, e.g.
-            ax.tick_params(axis='both', which='both', labelsize=16)
-            # format Y-axis
-            ax.yaxis.set_label_text(value['Y-label'])
-            ax.yaxis.label.set_size(16)
-            ax.yaxis.grid(True)
-            # format X-axis labels
-            ax.xaxis.set_label_text('Time [UTC]')
-            ax.xaxis.label.set_size(16)
-            # format MINOR X-axis ticks & labels
-            lacator_working = ax.xaxis.get_minor_locator()
-            ax.xaxis.set_minor_locator(mdates.HourLocator(interval=2))
-            ax.xaxis.set_minor_formatter(mdates.DateFormatter('%H:%M'))
-            ax.xaxis.grid(True, which="minor")
-            # format MINOR X-axis ticks & labels
-            ax.xaxis.set_major_locator(mdates.DayLocator())
-            ax.xaxis.set_major_formatter(mdates.DateFormatter('\n%d-%b\n%Y'))
-            plt.setp(ax.get_xticklabels(), rotation=0,
-                     horizontalalignment='center')
-            # clean & save
-            fig.tight_layout()
-        else:
-            fig, ax = plt.subplots()
-            textstr = "All Data returned\n      is NaN.\nNothing to plot!"
-            ax.text(0.2, 0.35, textstr, transform=ax.transAxes, fontsize=34)
+    conn = psycopg2.connect(('host={hostname} dbname={dbname} '
+                             'user={dbuser} password={dbpass}'
+                             ).format(**CONFIG['dbconn']))
+    cols = ["avg(%s) as avg_%s" % (v, v) for v in value['channels']]
+    cols = ", ".join(cols)
+    df = read_sql("""
+        SELECT tower, date_trunc('minute', valid) as ts,
+        """ + cols + """ from data_analog WHERE
+        valid between %s and %s
+        GROUP by tower, ts ORDER by ts ASC
+        """, conn, params=(time_data_start, time_now),
+                  index_col=None)
+    if len(df.index) > 0:
+        # === PLOT ===
+        fig, ax = plt.subplots(figsize=(17, 11))
+        for siteid in [0, 1]:
+            for col in value['channels']:
+                df2 = df[df['tower'] == siteid]
+                ax.plot(df2['ts'].values, df2["avg_" + col.lower()],
+                        label="%s %s" % (site_list[siteid], col))
+        # set legend and titles
+        lgnd = ax.legend(loc='best')
+        plot_title = ("One minute average of last {} hours of {} from BOTH"
+                      ).format(hours_back, key)
+        ax.set_title(plot_title, fontsize=22)
+        # set texts with times
+        fig.text(x=0.98, y=0.99, s='   generated at   \n' + ts_now_local,
+                 color='#888888', ha='right', va='top', fontsize=12)
+        ts_db_last = df.iloc[-1]['ts'].astimezone(
+            tz.gettz('UTC')).strftime('%d-%b-%y %H:%M UTC')
+        fig.text(x=0.98, y=0.01,
+                 s=('newest data:' + ts_db_last +
+                    '\nquery at: ' + ts_query_utc),
+                 color='#888888', ha='right', va='bottom', fontsize=10)
+        # set all tick parameters, e.g.
+        ax.tick_params(axis='both', which='both', labelsize=16)
+        # format Y-axis
+        ax.yaxis.set_label_text(value['Y-label'])
+        ax.yaxis.label.set_size(16)
+        ax.yaxis.grid(True)
+        # format X-axis labels
+        ax.xaxis.set_label_text('Time [UTC]')
+        ax.xaxis.label.set_size(16)
+        # format MINOR X-axis ticks & labels
+        lacator_working = ax.xaxis.get_minor_locator()
+        ax.xaxis.set_minor_locator(mdates.HourLocator(interval=2))
+        ax.xaxis.set_minor_formatter(mdates.DateFormatter('%H:%M'))
+        ax.xaxis.grid(True, which="minor")
+        # format MINOR X-axis ticks & labels
+        ax.xaxis.set_major_locator(mdates.DayLocator())
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('\n%d-%b\n%Y'))
+        plt.setp(ax.get_xticklabels(), rotation=0,
+                 horizontalalignment='center')
+        # clean & save
+        fig.tight_layout()
     else:
         fig, ax = plt.subplots()
         textstr = "No Data returned\n   from query!"
--- plots/sonic_monitor.py
+++ plots/sonic_monitor.py
@@ -2,8 +2,6 @@
 """
 make plot of recent data from talltowers project, for display on website.
 
-written for pyton 3
-
 @author: joe
 """
 import os
@@ -11,7 +9,7 @@ import json
 import datetime
 from dateutil import tz
 import psycopg2
-import pandas as pd
+from pandas.io.sql import read_sql
 import matplotlib
 matplotlib.use('Agg')  # Must be before importing matplotlib.pyplot.
 import matplotlib.pyplot as plt  # NOPEP8
@@ -19,7 +17,7 @@ import matplotlib.dates as mdates  # NOPEP8
 # get database credentials
 CONFIG = json.load(open("../config/settings.json", 'r'))
 
-site_list = ['story', 'hamilton']
+site_list = ['hamilton', 'story']
 hours_back = 336
 hour_interval = 24  # period of "%H:%M" tickmarks in hours
 plot_dir = CONFIG['plotsdir']
@@ -49,7 +47,7 @@ plot_dict = {'Sonic R.H.': {
              }
 
 for key, value in plot_dict.items():
-    for site in site_list:
+    for siteid, site in enumerate(site_list):
         time_now = datetime.datetime.utcnow().replace(tzinfo=tz.gettz('UTC'))
         time_data_start = time_now - datetime.timedelta(hours=hours_back)
         # timestamps for plotting
@@ -63,88 +61,60 @@ for key, value in plot_dict.items():
         conn = psycopg2.connect(('host={hostname} dbname={dbname} '
                                  'user={dbuser} password={dbpass}'
                                  ).format(**CONFIG['dbconn']))
-        cur = conn.cursor()
-        for chn in value['channels']:
-            # get chn_id
-            cur.execute("""SELECT chn_id
-                           FROM Channels
-                           WHERE site = %s
-                           AND header = %s;""", (site, chn))
-            chn_id = cur.fetchall()[0][0]
-            # get data
-            #   note: datetime.datetime objects with a tz, will be
-            #       converted to postgres's "TIMESTAMPTZ" format
-            cur.execute("""SELECT date_trunc('minute', ts), AVG(val)
-                        FROM Dat
-                        WHERE chn_id = %s
-                        AND ts > %s
-                        AND ts < %s
-                        GROUP BY date_trunc('minute', ts)
-                        ORDER BY date_trunc('minute', ts)
-                        """, (chn_id, time_data_start, time_now))
-            data = cur.fetchall()
-            # make pandas.DataFrame object
-            df_raw = pd.DataFrame(data, columns=['ts',
-                                                 chn]).set_index('ts')
-            df_raw.columns = ["_".join([site.capitalize(),
-                                        df_raw.columns[0]])]
-            dfs.append(df_raw)
-        # ===================== PLOT DATA, IF ANY =======================
-        # verify that data was returned
-        if any([len(df_) > 0 for df_ in dfs]):
-            # combine all channels into one data frame
-            df = pd.concat(dfs, axis=1)
-            # verify that dat is not all NANs
-            if len(df.dropna(how='all')) > 0:
-                # === PLOT ===
-                fig, ax = plt.subplots(figsize=(17, 11))
-                ts = df.index
-                for col in df:
-                    ax.plot(ts, df[col], label=col)
-                # set legend and titles
-                lgnd = ax.legend(loc='best')
-                plot_title = ("One minute average of last {} hours "
-                              "of {} from {}"
-                              ).format(hours_back, key, site.upper())
-                ax.set_title(plot_title, fontsize=22)
-                # set texts with times
-                fig.text(x=0.98, y=0.99,
-                         s='   generated at   \n' + ts_now_local,
-                         color='#888888', ha='right', va='top', fontsize=12)
-                ts_db_last = df.index[-1].astimezone(
-                    tz.gettz('UTC')).strftime('%d-%b-%y %H:%M UTC')
-                fig.text(x=0.98, y=0.01,
-                         s=('newest data:' + ts_db_last +
-                            '\nquery at: ' + ts_query_utc),
-                         color='#888888', ha='right', va='bottom', fontsize=10)
-                # set all tick parameters, e.g.
-                ax.tick_params(axis='both', which='both', labelsize=16)
-                # format Y-axis
-                ax.yaxis.set_label_text(value['Y-label'])
-                ax.yaxis.label.set_size(16)
-                ax.yaxis.grid(True)
-                # format X-axis labels
-                ax.xaxis.set_label_text('Time [UTC]')
-                ax.xaxis.label.set_size(16)
-                # format MINOR X-axis ticks & labels
-                lacator_working = ax.xaxis.get_minor_locator()
-                ax.xaxis.set_minor_locator(
-                    mdates.HourLocator(interval=hour_interval))
-                ax.xaxis.set_minor_formatter(mdates.DateFormatter('%H:%M'))
-                ax.xaxis.grid(True, which="minor")
-                # format MINOR X-axis ticks & labels
-                ax.xaxis.set_major_locator(mdates.DayLocator())
-                ax.xaxis.set_major_formatter(
-                    mdates.DateFormatter('\n%d-%b\n%Y'))
-                plt.setp(ax.get_xticklabels(), rotation=0,
-                         horizontalalignment='center')
-                # clean & save
-                fig.tight_layout()
-            else:
-                fig, ax = plt.subplots()
-                textstr = "All Data returned\n      is NaN.\nNothing to plot!"
-                ax.text(0.2, 0.35, textstr, transform=ax.transAxes,
-                        fontsize=34)
+        cols = ["avg(%s) as avg_%s" % (v, v) for v in value['channels']]
+        cols = ", ".join(cols)
+        df = read_sql("""
+            SELECT date_trunc('minute', valid) as ts,
+            """ + cols + """ from data_monitor WHERE
+            tower = %s and valid between %s and %s
+            GROUP by ts ORDER by ts ASC
+            """, conn, params=(siteid, time_data_start, time_now),
+                      index_col='ts')
+        if len(df.index) > 0:
+            # === PLOT ===
+            fig, ax = plt.subplots(figsize=(17, 11))
+            ts = df.index
+            for col in df:
+                ax.plot(ts, df[col], label=col)
+            # set legend and titles
+            lgnd = ax.legend(loc='best')
+            plot_title = ("One minute average of last {} hours "
+                          "of {} from {}"
+                          ).format(hours_back, key, site.upper())
+            ax.set_title(plot_title, fontsize=22)
+            # set texts with times
+            fig.text(x=0.98, y=0.99,
+                     s='   generated at   \n' + ts_now_local,
+                     color='#888888', ha='right', va='top', fontsize=12)
+            ts_db_last = df.index[-1].astimezone(
+                tz.gettz('UTC')).strftime('%d-%b-%y %H:%M UTC')
+            fig.text(x=0.98, y=0.01,
+                     s=('newest data:' + ts_db_last +
+                        '\nquery at: ' + ts_query_utc),
+                     color='#888888', ha='right', va='bottom', fontsize=10)
+            # set all tick parameters, e.g.
+            ax.tick_params(axis='both', which='both', labelsize=16)
+            # format Y-axis
+            ax.yaxis.set_label_text(value['Y-label'])
+            ax.yaxis.label.set_size(16)
+            ax.yaxis.grid(True)
+            # format X-axis labels
+            ax.xaxis.set_label_text('Time [UTC]')
+            ax.xaxis.label.set_size(16)
+            # format MINOR X-axis ticks & labels
+            lacator_working = ax.xaxis.get_minor_locator()
+            ax.xaxis.set_minor_locator(
+                mdates.HourLocator(interval=hour_interval))
+            ax.xaxis.set_minor_formatter(mdates.DateFormatter('%H:%M'))
+            ax.xaxis.grid(True, which="minor")
+            # format MINOR X-axis ticks & labels
+            ax.xaxis.set_major_locator(mdates.DayLocator())
+            ax.xaxis.set_major_formatter(
+                mdates.DateFormatter('\n%d-%b\n%Y'))
+            plt.setp(ax.get_xticklabels(), rotation=0,
+                     horizontalalignment='center')
+            # clean & save
+            fig.tight_layout()
         else:
             fig, ax = plt.subplots()
             textstr = "No Data returned\n   from query!"
