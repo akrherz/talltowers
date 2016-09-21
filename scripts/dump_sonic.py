941f9ec7c280fc371d992cf8fb27762ed15023a1
akrherz <akrherz@iastate.edu>
2016-09-21 15:39:58 -0500

    another dumper

---
+++ scripts/dump_sonic.py
@@ -0,0 +1,22 @@
+import datetime
+import psycopg2
+from ansible.modules.extras.cloud.amazon import sts_assume_role
+
+pgconn = psycopg2.connect(database='talltowers',
+                          host='talltowers-db.agron.iastate.edu',
+                          user='tt_web')
+cursor = pgconn.cursor()
+
+tower = 1
+cursor.execute("""
+    SELECT tower,
+        valid at time zone 'America/Chicago' as localvalid,
+    *
+    from data_sonic WHERE tower = %s and
+    valid between '2016-07-06 05:00+00' and '2016-07-07 05:00+00'
+    ORDER by localvalid""" % (tower,))
+o = open('story_sonic.csv', 'w')
+for row in cursor:
+    o.write(",".join([str(s) for s in row]) + "\n")
+
+o.close()
