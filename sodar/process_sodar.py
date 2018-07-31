6b199e66a5e5d8e6b96d715fe33188882848b23b
akrherz <akrherz@iastate.edu>
2018-07-31 13:25:33 -0500

    add ingest and database storage of SODAR data

---
+++ sodar/process_sodar.py
@@ -0,0 +1,158 @@
+"""Process the SODAR files.
+
+data starts 2018-05-24-17-40
+cd 963/
+-rw-rw-rw- triton_963_2018-07-31-16-50_extended.csv
+-rw-rw-rw- triton_963_2018-07-31-16-50_operational.csv
+-rw-rw-rw- triton_963_2018-07-31-16-50_standard.csv
+"""
+import re
+import os
+import sys
+import datetime
+import json
+from ftplib import FTP
+
+import pandas as pd
+import pytz
+import psycopg2
+COLRE = re.compile("^(?P<elev>[0-9]+)m (?P<name>.*?)-?(?P<extra>[ABC])?$")
+CONFIG = json.load(open("../config/settings.json", 'r'))
+PGCONN = psycopg2.connect(('host={hostname} dbname={dbname} '
+                           'user={dbuser} password={dbpass}'
+                           ).format(**CONFIG['dbconn']))
+
+
+def nice(val):
+    """Make sure this value is nice"""
+    if pd.isna(val):
+        return None
+    return val
+
+
+def dbsave(df, valid):
+    """Save to the database, a dataframe"""
+    table = "sodar_profile" if "beamnum" in df.columns else "sodar_surface"
+    cursor = PGCONN.cursor()
+    cursor.execute("""
+    DELETE from """ + table + """ WHERE station = 963 and valid = %s
+    """, (valid, ))
+    if table == "sodar_surface":
+        cols = ['station', 'valid']
+        args = ['963', valid]
+        row = df.iloc[0]
+        for col in df.columns:
+            args.append(nice(row[col]))
+            cols.append(col)
+        cols = ",".join(cols)
+        vals = ",".join(["%s"] * len(args))
+        cursor.execute("""
+            INSERT into """ + table + """(""" + cols + """)
+            VALUES (""" + vals + """)
+        """, args)
+    else:
+        for _, row in df.iterrows():
+            cols = ['station', 'valid']
+            args = ['963', valid]
+            for col in df.columns:
+                args.append(nice(row[col]))
+                cols.append(col)
+            cols = ",".join(cols)
+            vals = ",".join(["%s"] * len(args))
+            sql = """
+                INSERT into """ + table + """(""" + cols + """)
+                VALUES (""" + vals + """)
+            """
+            cursor.execute(sql, args)
+
+    cursor.close()
+    PGCONN.commit()
+
+
+def strcol(col):
+    """Regularize"""
+    return col.lower().replace("-", "_").replace(" ", "_")
+
+
+def translate_column(col):
+    """Turn the column name into actionable stuff"""
+    m = COLRE.match(col)
+    if m is None:
+        return [None, strcol(col), None]
+    res = m.groupdict()
+    return [res['elev'], strcol(res['name']), res['extra']]
+
+
+def ingest(valid):
+    """Process the files."""
+    dfs = []
+    for suffix in ['extended', 'operational', 'standard']:
+        fn = valid.strftime(
+            "/home/sodar/triton_963_%Y-%m-%d-%H-%M_" + suffix + ".csv"
+        )
+        df = pd.read_csv(fn)
+        df.set_index(df.columns[0], inplace=True)
+        dfs.append(df)
+    df = dfs[0].join(dfs[1:])
+    surface = {}
+    profiles = []
+    for col in df.columns:
+        res = translate_column(col)
+        val = df.iloc[0][col]
+        if res[0] is None:
+            surface[res[1]] = val
+        else:
+            profiles.append(
+                {'labelin': "%s_%s" % (res[0],
+                                       res[2] if res[2] is not None else '0'),
+                 'colname': res[1],
+                 'value': val
+                 }
+            )
+    profile = pd.DataFrame(profiles)
+    profile2 = profile.pivot(
+        index='labelin',
+        values='value',
+        columns='colname'
+    )
+    profile2.reset_index(inplace=True)
+    profile2['height'] = profile2['labelin'].str.split("_").str.get(0)
+    profile2['label'] = profile2['labelin'].str.split("_").str.get(1)
+    profile2.drop('labelin', axis=1, inplace=True)
+    dbsave(profile2, valid)
+    surface = pd.DataFrame([surface])
+    dbsave(surface, valid)
+
+
+def download_files(valid):
+    """Get the files for this valid time."""
+    settings = json.loads(open('secret.json').read())
+    conn = None
+    for suffix in ['extended', 'operational', 'standard']:
+        remotefn = valid.strftime(
+            "963/triton_963_%Y-%m-%d-%H-%M_" + suffix + ".csv"
+        )
+        localfn = "/home/sodar/%s" % (remotefn[4:], )
+        if os.path.isfile(localfn):
+            continue
+        if conn is None:
+            conn = FTP(settings['host'])
+            conn.login(settings['username'], settings['password'])
+        fp = open(localfn, 'wb')
+        conn.retrbinary('RETR %s' % (remotefn, ), fp.write)
+        fp.close()
+    if conn:
+        conn.close()
+
+
+def main(argv):
+    """Go Main"""
+    valid = datetime.datetime(int(argv[1]), int(argv[2]), int(argv[3]),
+                              int(argv[4]), int(argv[5]))
+    valid = valid.replace(tzinfo=pytz.UTC)
+    download_files(valid)
+    ingest(valid)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
