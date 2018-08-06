002ac00d354727f68e9f2b04d72fa1cf427899a7
akrherz <akrherz@iastate.edu>
2018-08-06 13:05:33 -0500

    sync laptop changes

---
+++ plots/event.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Very similar to pg-www-plot.py, but plots data from related channels,
+at both sites, on one plot
+
+make plot of recent data from talltowers project, for display on website.
+
+@author: joe
+"""
+import os
+import json
+import datetime
+import pandas as pd
+from dateutil import tz
+import psycopg2
+from pandas.io.sql import read_sql
+import matplotlib
+matplotlib.use('agg')  # Must be before importing matplotlib.pyplot.
+import matplotlib.pyplot as plt  # NOPEP8
+import matplotlib.dates as mdates  # NOPEP8
+import pytz
+
+TZ = pytz.timezone("America/Chicago")
+
+
+def get_data():
+    conn = psycopg2.connect(database="talltowers", host='talltowers-db.agron.iastate.edu',
+                            user='tt_web')
+
+    df = read_sql("""
+        SELECT * from data_analog WHERE
+        valid between '2017-05-01 00:00+00' and '2017-05-01 02:00:00+00'
+        and tower = 0 ORDER by valid ASC
+        """, conn, index_col='valid')
+    df.to_csv('data.csv')
+
+
+df = pd.read_csv('data.csv')
+df['valid'] = pd.to_datetime(df['valid'])
+df.set_index('valid', inplace=True)
+
+fig, (ax, ax2) = plt.subplots(2, 1, figsize=(8, 6), sharex=True)
+ax.plot(df.index.values, df['bp_10m'])
+ax.set_title(("ISU Atmospheric Observatory :: Hamilton County Tall Tower\n"
+              "10 meter AGL 1hz data on evening of 30 April 2017"))
+ax.set_ylabel("Air Pressure [mb]")
+ax22 = ax2.twinx()
+ax22.plot(df.index.values, df['ws_20m_s'], zorder=3)
+ax2.scatter(df.index.values, df['winddir_20m_s'], color='r', zorder=2)
+ax2.set_yticks(range(0, 361, 90))
+ax2.set_yticklabels(["N", "E", "S", "W", "N"])
+ax2.set_ylim(0, 360)
+ax2.set_ylabel("Wind Direction", color='r')
+ax22.set_ylim(0, 20)
+ax22.set_yticks([0, 5, 10, 15, 20])
+ax22.set_ylabel("Wind Speed [mps]", color='b')
+# format MINOR X-axis ticks & labels
+ax.xaxis.set_major_locator(mdates.MinuteLocator(byminute=[0, 15, 30, 45], tz=TZ))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%-I:%M %p', tz=TZ))
+ax.grid(True)
+ax2.grid(True)
+ax2.set_xlabel("30 April 2017 (Central Daylight Time)")
+# save figure
+fig.savefig('test.png')
+plt.close()
---
+++ plots/rh.py
@@ -0,0 +1,56 @@
+import psycopg2
+import matplotlib.pyplot as plt
+from pandas.io.sql import read_sql
+import datetime
+import numpy as np
+from scipy.interpolate import NearestNDInterpolator
+
+pgconn = psycopg2.connect(host='talltowers-db.agron.iastate.edu',
+                          user='tt_web', database='talltowers')
+v = "uz"
+df = read_sql("""
+ SELECT date_trunc('microsecond', valid) as ts, avg("""+v+"""_5m) as avg_rh_5m,
+ avg("""+v+"""_10m) as avg_rh_10m, avg("""+v+"""_20m) as avg_rh_20m,
+ avg("""+v+"""_40m) as avg_rh_40m,
+ avg("""+v+"""_80m) as avg_rh_80m, avg("""+v+"""_120m) as avg_rh_120m
+ from data_sonic WHERE tower = 0 and valid > '2017-05-01 00:00:00+00'
+ and valid < '2017-05-01 02:00:00+00'
+ GROUP by ts ORDER by ts ASC""", pgconn, index_col='ts')
+print len(df.index)
+x = []
+y = []
+z = []
+ts0 = df.index[0]
+print ts0
+for i, row in df.iterrows():
+    for level in [5, 10, 20, 40, 80, 120]:
+        x.append((i - ts0).total_seconds())
+        y.append(level)
+        v = row["avg_rh_%sm" % (level,)]
+        z.append(v)
+
+x = np.array(x)
+y = np.array(y)
+z = np.array(z)
+(fig, ax) = plt.subplots(1, 1, figsize=(12, 6))
+
+
+xi = np.linspace(min(x), max(x), 1000)
+xticks = []
+xticklabels = []
+for xx in xi[::100]:
+    ts = ts0 + datetime.timedelta(seconds=xx)
+    xticks.append(xx)
+    xticklabels.append(ts.strftime("%H:%M:%S"))
+yi = np.linspace(0, 120, 10)
+xi, yi = np.meshgrid(xi, yi)
+nn = NearestNDInterpolator((x, y), z)
+vals = nn(xi, yi)
+
+res = ax.contourf(xi, yi, vals)
+ax.set_xticks(xticks)
+ax.set_xticklabels(xticklabels, rotation=15)
+ax.grid()
+ax.set_title("Story Tower - 11 Aug 2016 ~1140 UTC~ Sonic Vertical Wind Speed")
+fig.colorbar(res, label='mps')
+fig.savefig('test.png')
